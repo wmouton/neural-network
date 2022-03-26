1408eb4410f539f0c9e52b6dcfedab8957480f3e
l33th <l33th@protonmail.ch>
2022-03-26 05:52:46 +0200

    tensorflow placeholders and variables

--- neural_network.py
+++ neural_network.py
@@ -37,6 +37,17 @@ def main():
     W3_init = np.random.randn(M2, K) / np.sqrt(M2)
     b3_init = np.zeros(K)
 
+    Z = tf.placeholder(tf.float32, shape=(None, D), name='X')
+    T = tf.placeholder(tf.float32, shape=(None, K), name='T')
+    W1 = tf.Variable(W1_init.astype(np.float32))
+    b1 = tf.Variable(b1_init.astype(np.float32))
+    W2 = tf.Variable(W2_init.astype(np.float32))
+    b2 = tf.Variable(b2_init.astype(np.float32))
+    W3 = tf.Variable(W3_init.astype(np.float32))
+    b3 = tf.Variable(b3_init.astype(np.float32))
+
+
+
 
 
 if __name__ == '__main__':
