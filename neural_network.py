9d6185dd510b86f615f54fdbaf6f518813eccf3c
l33th <l33th@protonmail.ch>
2022-03-26 05:48:20 +0200

    some layers

--- neural_network.py
+++ neural_network.py
@@ -11,6 +11,33 @@ def error_rate(p, t):
 def main():
     X, Y = get_normalized_data()
 
+    max_iter = 30
+    print_period = 10
+    lr = 0.00004
+    reg = 0.01
+
+    Xtrain = X[:-1000,]
+    Ytrain = Y[:-1000]
+    Xtest = X[-1000:,]
+    Ytest = Y[-1000:]
+    Ytrain_ind = y2indicator(Ytrain)
+    Ytest_ind = y2indicator(Ytest)
+
+    N, D = Xtrain.shape
+    batch_sz = 500
+    n_batches = N / batch_sz
+
+    M1 = 300
+    M2 = 100
+    K = 10
+    W1_init = np.random.randn(D, M1) / 28
+    b1_init = np.zeros(M1)
+    W2_init = np.random.randn(M1, M2) / np.sqrt(M1)
+    b2_init = np.zeros(M2)
+    W3_init = np.random.randn(M2, K) / np.sqrt(M2)
+    b3_init = np.zeros(K)
+
+
 
 if __name__ == '__main__':
     main()
